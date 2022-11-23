f0e0438d5e7429e1a646e8daadee908fb5a1689e
Marian Dobre <dobremrn12@gmail.com>
2022-11-24 00:22:54 +0200

    created header component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import { Signup } from "./pages/Signup";
 import { Offers } from "./pages/Offers";
 import { ForgotPassword } from "./pages/ForgotPassword";
 import { Profile } from "./pages/Profile";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
