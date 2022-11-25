03f173113488ac149c42fe4cccc1182a31b74104
Marian Dobre <dobremrn12@gmail.com>
2022-11-25 22:36:53 +0200

    created spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import spinner from "../assets/svg/spinner.svg";
+
+export const Spinner = () => {
+  return (
+    <div>
+      <div className="bg-black bg-opacity-50 flex items-center justify-center fixed left-0 right-0 bottom-0 top-0 z-50">
+        <img src={spinner} alt="Loading..." className="h-15" />
+      </div>
+    </div>
+  );
+};
